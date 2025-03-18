54857de7d40939f341e52efc00bacba27c158887
Anuka Shadow <141473378+AnukaSamuditha@users.noreply.github.com>
2025-03-18 13:34:51 +0530

    Database connection method has been added

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_DB_URL)
+.then(()=>{
+    console.log("Database connected successfully");
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on ${process.env.PORT}`);
+    })
+}).catch((error)=>{
+    console.log("Error connecting with the database",error);
+})
