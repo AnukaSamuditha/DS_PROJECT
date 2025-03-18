6043fe4499c6f00188fc9a4f121ac5fe508196af
Anuka Shadow <141473378+AnukaSamuditha@users.noreply.github.com>
2025-03-18 21:41:23 +0530

    Map(Google Map) has been added to the system

--- food_ordering_system/src/Home.jsx
+++ food_ordering_system/src/Home.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import GoogleMapContainer from "./components/GoogleMapContainer";
 
 export default function Home(){
     return(
         <section>
             <h1 className="text-3xl font-bold text-yellow-600">This is home</h1>
+            <GoogleMapContainer/>
         </section>
     )
 }
