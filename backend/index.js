7374f7707abe01d4bfa69e51fb7545ee29223f57
Anuka Shadow <141473378+AnukaSamuditha@users.noreply.github.com>
2025-03-20 04:35:24 +0530

    authentication methods has been added

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_DB_URL)
-.then(()=>{
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
     console.log("Database connected successfully");
-    app.listen(process.env.PORT,()=>{
-        console.log(`Server is running on ${process.env.PORT}`);
-    })
-}).catch((error)=>{
-    console.log("Error connecting with the database",error);
-})
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting with the database", error);
+  });
