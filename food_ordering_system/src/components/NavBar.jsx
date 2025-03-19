cf041b3708622954c48e41055b1e7e9f868792fe
Anuka Shadow <141473378+AnukaSamuditha@users.noreply.github.com>
2025-03-20 01:17:26 +0530

    Updated the home screen

---
+++ food_ordering_system/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router";
+
+export default function NavBar(){
+    return(
+        <header className="flex sticky top-0 z-[100] w-full h-[60px] justify-between lg:justify-center items-center border-b border-zinc-800 backdrop-blur bg-transparent">
+            <ul className="w-full flex justify-center items-center gap-10 font-semibold text-white text-sm">
+                <li><NavLink>Home</NavLink></li>
+                <li><NavLink>Restaurants</NavLink></li>
+                <li><NavLink>Food</NavLink></li>
+                <li><NavLink>Contact</NavLink></li>
+            </ul>
+        </header>
+    )
+}
