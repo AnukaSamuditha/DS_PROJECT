973db2eb7d0c781d36f3c080744ad2985c88a9fd
Anuka Shadow <141473378+AnukaSamuditha@users.noreply.github.com>
2025-03-20 10:49:15 +0530

    user routes and user controller and user model has been added

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const userRoutes = require("./Routes/userRoutes");
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+app.use("/users",userRoutes);
 
 mongoose
   .connect(process.env.MONGO_DB_URL)
