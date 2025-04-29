0d0c2596f26b26e39d0a5623e33696114f4d1eeb
Anuka Shadow <141473378+AnukaSamuditha@users.noreply.github.com>
2025-04-29 17:53:22 +0530

    Structure created for microservices architecture and Docker

--- backend/Routes/product.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {createProduct,getAllProducts,getAProduct} = require("../Controllers/product.controller");
-
-router.post("/",createProduct);
-router.get("/",getAllProducts);
-router.get("/:id",getAProduct);
-
-module.exports = router;
